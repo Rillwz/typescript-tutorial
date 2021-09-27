4b56b72d14ebf71f238c41de229fbd49df76372f
Rillwz <Yusril.edmodo25@gmail.com>
2021-09-28 04:37:11 +0700

    typescript

---
+++ sandbox.js
@@ -0,0 +1,8 @@
+var character = "Yusril";
+var age = 30;
+var isHappy = true;
+character = "lluihin";
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(0.1));
---
+++ sandbox.ts
@@ -0,0 +1,11 @@
+let character = "Yusril";
+let age = 30;
+let isHappy = true;
+
+character = "lluihin";
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
+};
+
+console.log(circ(0.1));
