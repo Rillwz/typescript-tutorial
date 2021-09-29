a09f897f8eba559e9a174239718569fa8ac260e8
Rillwz <Yusril.edmodo25@gmail.com>
2021-09-29 09:46:52 +0700

    Classes

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,38 @@
 "use strict";
-console.log("test");
+// Functions basics
+let greet;
+greet = () => {
+    console.log("hello, again!");
+};
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+};
+add(5, 10, "20");
+const minus = (a, b) => {
+    return a + b;
+};
+let result = minus(10, 7);
+const users = (user) => {
+    console.log(`${user.name} says hello!`);
+};
+// -- Function Signatures
+// example 1
+let gree;
+gree = (name, greeting) => {
+    console.log(`${name} says ${greeting}`);
+};
+// example 2
+let calc;
+calc = (numOne, numTwo, action) => {
+    if (action === "add") {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
+};
+// example 3
+let logDetails;
+logDetails = (ninja) => {
+    console.log(`${ninja.name} is ${ninja.age} years old`);
+};
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,26 @@
+// classes
+
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+
+  constructor(c: string, d: string, a: number) {
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+
+  format() {
+    return `${this.client} owes ${this.amount} for ${this.details}`;
+  }
+}
+
+const invOne = new Invoice("Jett", "work in huge mountain", 300);
+const invTwo = new Invoice("Phoenix", "work in fire goverment", 300);
+
+console.log(invOne, invTwo);
+
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 console.log(form.children);
 
